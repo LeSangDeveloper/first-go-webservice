b262dbf1802b11c16fc24e661964ca6908de4d38
Le Sang <lesang541191309@gmail.com>
2022-01-31 18:15:16 +0700

    setup Database

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"net/http"
+	"pluralsight/gowebservice/database"
 	"pluralsight/gowebservice/product"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
